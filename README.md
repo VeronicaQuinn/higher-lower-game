cc6422b69189eae9829595b6c82e570ac494d961
VeronicaQuinn <veronica.quinn@gmail.com>
2022-03-05 18:50:58 +0000

    Change method of creating deck array in order to coerce strings into number values

---
+++ README.md
@@ -0,0 +1,1 @@
+![image](https://user-images.githubusercontent.com/95372009/156895637-e2e59f72-ccb9-4757-8224-ee8b80772f95.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,64 @@
 // Waits for DOM to load before running any other functions
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-// HTML elements
+    // HTML elements
 
-const startButton = document.getElementById('start-game');
-const pileTwo = document.getElementById('right-card');
-const pileOne = document.getElementById('left-card');
-const buttons = document.getElementsByClassName('button');
+    const startButton = document.getElementById('start-game');
+    const pileTwo = document.getElementById('right-card');
+    const pileOne = document.getElementById('left-card');
+    const buttons = document.getElementsByClassName('button');
 
-// Game play variables
+    // Game play variables
 
-const suits = ['&hearts;', '&spades;', '&diams;', '&clubs;'];
-const faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
-const deck = [];
-let currentCard;
-let newCard;
+    const suits = ['&hearts;', '&spades;', '&diams;', '&clubs;'];
+    const faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+    const deck = [];
+    let currentCard;
+    let newCard;
 
-startButton.addEventListener('click', startGame);
+    startButton.addEventListener('click', startGame);
 
-// Create deck
+    // Create deck
 
-for (let suit in suits) {
-    for (let face in faces) {
-        deck.push(faces[face] + suits[suit]);
+    for (let suit of suits) {
+    for (let face of faces) {
+        switch (face) {
+            case "A":
+                deck.push({value: 14, card: `${face}${suit}`});
+                break;
+            case "K":
+                deck.push({value: 13, card: `${face}${suit}`});
+                break;
+            case "Q":
+                deck.push({value: 12, card: `${face}${suit}`});
+                break;
+            case "J":
+                deck.push({value: 11, card: `${face}${suit}`});
+                break;
+            default:
+                deck.push({value: face, card: `${face}${suit}`});
+        }
     }
 }
 
-function startGame() {
-    // Shuffle deck
-    for (let i = 0; i < 1000; i++) {
-        let shuffle1 = Math.floor((Math.random() * deck.length));
-        let shuffle2 = Math.floor((Math.random() * deck.length));
-        let shuffled = deck[shuffle1];
-        deck[shuffle1] = deck[shuffle2];
-        deck[shuffle2] = shuffled;
-    }
-    console.log(deck);
-    pileOne.innerHTML = deck.pop();
-    startButton.remove();
+    function startGame() {
+        // Shuffle deck
+        for (let i = 0; i < 1000; i++) {
+            let shuffle1 = Math.floor((Math.random() * deck.length));
+            let shuffle2 = Math.floor((Math.random() * deck.length));
+            let shuffled = deck[shuffle1];
+            deck[shuffle1] = deck[shuffle2];
+            deck[shuffle2] = shuffled;
+        }
+        currentCard = deck.pop();
+    pileOne.innerHTML = currentCard.card;
+        startButton.remove();
 
-    for (let button of buttons) {
-        button.addEventListener('click', guess);
-    }
+        for (let button of buttons) {
+            button.addEventListener('click', guess);
+        }
     };
-    })
+
+    
+})
